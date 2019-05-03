4030c45767ce2d35acadea14be3592f11163ebf3
jagan <jagandravid7@gmail.com>
2019-05-03 21:44:12 +0530

    form error without data

--- src/client/app/components/customer/customer.component.ts
+++ src/client/app/components/customer/customer.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl
+} from '@angular/forms';
 /**
  * This class represents the lazy loaded AboutComponent.
  */
@@ -8,7 +14,7 @@ import { Component, OnInit, Input } from '@angular/core';
   templateUrl: 'customer.component.html',
   styleUrls: ['customer.component.css']
 })
-export class CustomerComponent {
+export class CustomerComponent implements OnInit{
   visibleOne: Boolean = false;
   visibleTwo: Boolean = true;
 
@@ -22,8 +28,76 @@ export class CustomerComponent {
   newcaseOpen:Boolean = false;
   newdriverOpen:Boolean = false;
 
-  @Input() customer: any;
+  customerForm: FormGroup;
+  customerdata:any;
 
+  @Input() customer: any;
+  constructor(private formBuilder: FormBuilder) {}
+  ngOnInit(){
+    this.customerForm = this.formBuilder.group({
+      name:[''],
+      accountid:[''],
+      title:[''],
+      birthdate:[''],
+      record_type:[''],
+      do_not_contact__c:[''],
+      business_phone__c:[''],
+      business_phone_service_type__c:[''],
+      business_email_valid__c:[''],
+      business_phone_valid__c:[''],
+      home_email__c:[''],
+      homephone:[''],
+      home_phone_service_type__c:[''],
+      home_phone_valid__c:[''],
+      mobilephone:[''],
+      interest_of_make__c:[''],
+      billing_address__c:[''],
+      billing_suburb__c:[''],
+      billing_post_code__c:[''],
+      billing_country__c:[''],
+      billingdpid__c:[''],
+      billing_latitude__c:[''],
+      billing_longitude__c:[''],
+      first_name__c: ['', Validators.required],
+      last_name__c: ['', Validators.required],
+      customer_type__c:[''],
+      parent_account: [''],
+      accountnumber: [''],
+      account_record: [''],
+      selling_dealer: [''],
+      service_dealer: [''],
+      customer_type: [''],
+      fax: [''],
+      phone1__c: [''],
+      business_email__c: [''],
+      phone1_service_type__c: [''],
+      phone1_valid__c: [''],
+      email: [''],
+      phone2_service_type__c: [''],
+      phone2_valid__c: [''],
+      web_site: [''],
+      phone: [''],
+      billing_address: [''],
+      billing_subrub: [''],
+      billing_state__c: [''],
+      billing_postcode__c: [''],
+      billing_country: [''],
+      billing_dpid: [''],
+      billing_lat: [''],
+      billing_lng: [''],
+      physical_address__c: [''],
+      physical_state__c: [''],
+      physical_post_code__c: [''],
+      physical_country__c: [''],
+      physicaldpid__c: [''],
+      physical_latitude__c: [''],
+      physical_longitude__c: [''],
+      license_number__c: [''],
+      license_expiry_date__c: [''],
+      numberoflocations__c: [''],
+      phone2__c: ['']
+    });
+  }
   addnewCase(){
     this.newcaseOpen = true;
   }
