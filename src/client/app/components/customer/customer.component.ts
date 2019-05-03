b7dc1bb361516758ad1459b8da4eed12c93f6736
Mukeshkumar <mukesh@infohawk.in>
2019-05-03 20:36:33 +0530

    driver and customer retails page

--- src/client/app/components/customer/customer.component.ts
+++ src/client/app/components/customer/customer.component.ts
@@ -12,5 +12,35 @@ export class CustomerComponent {
   visibleOne: Boolean = false;
   visibleTwo: Boolean = true;
 
+  visible1: Boolean = false;
+  visible2: Boolean = true;
+  visible3: Boolean = true;
+
+  visibleDetail: Boolean = true;
+  visibleRelated: Boolean = false;
+
+  newcaseOpen:Boolean = false;
+  newdriverOpen:Boolean = false;
+
   @Input() customer: any;
+
+  addnewCase(){
+    this.newcaseOpen = true;
+  }
+  addnewDriver(){
+    this.newdriverOpen = true;
+  }
+  close(){
+    this.newcaseOpen = false;
+    this.newdriverOpen = false;
+  }
+  viewDetail(){
+    this.visibleDetail = true;
+    this.visibleRelated = false;
+  }
+  viewRelated(){
+    this.visibleDetail = false;
+    this.visibleRelated = true;
+  }
+
 }
