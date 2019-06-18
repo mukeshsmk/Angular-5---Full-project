71d5e005a4d02061c74a57a0f3e6853445617537
jagan <jagandravid7@gmail.com>
2019-06-18 14:08:24 +0530

    quick lead invalid issue/tooltip in main component

--- src/app/components/table-list/table-list.module.ts
+++ src/app/components/table-list/table-list.module.ts
@@ -1,35 +1,37 @@
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { TableListComponent } from './table-list.component';
-import { TableListRoutingModule } from './table-list-routing.module';
-import { TabsComponent } from '../tab/tabs.component';
-import { TabComponent } from '../tab/tab.component';
-import { DynamicTabsDirective } from '../tab/dynamic-tabs.directive';
-import { CommonModule } from '@angular/common';
-import { UserdetailsComponent } from '../user-details/userdetails.component';
-import { OpportunityComponent } from '../opportunity/opportunity.component';
-import { DriverComponent } from '../driver/driver.component';
-import { CustomerComponent } from './../customer/customer.component';
-import { StockComponent } from './../stock/stock.component';
-import { OpportunityFormComponent } from './../opportunity-form/opportunity-form.component';
-import { DriverFormComponent } from './../driver-form/driver-form.component';
-import { CustomerFormComponent } from './../customer-form/customer-form.component';
-import { VehicleStockFormComponent } from './../vehiclestock-form/vehiclestock-form.component';
-import { NavFormComponent } from './../nav-form/nav-form.component';
-import { ActivitiesComponent } from './../activities/activities.component';
-import { NavigationBarComponent } from './../navigation-bar/navigation-bar.component';
-import { DashboardComponent } from './../dashboard/dashboard.component';
-import { ProfileComponent } from '../profile/profile.component';
-import { GeneralService } from '../../shared/services/GeneralService';
-import { UserDairyModule } from '../calendar/user-dairy.module';
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { TableListComponent } from "./table-list.component";
+import { TableListRoutingModule } from "./table-list-routing.module";
+import { TabsComponent } from "../tab/tabs.component";
+import { TabComponent } from "../tab/tab.component";
+import { DynamicTabsDirective } from "../tab/dynamic-tabs.directive";
+import { CommonModule } from "@angular/common";
+import { UserdetailsComponent } from "../user-details/userdetails.component";
+import { OpportunityComponent } from "../opportunity/opportunity.component";
+import { DriverComponent } from "../driver/driver.component";
+import { CustomerComponent } from "./../customer/customer.component";
+import { StockComponent } from "./../stock/stock.component";
+import { OpportunityFormComponent } from "./../opportunity-form/opportunity-form.component";
+import { DriverFormComponent } from "./../driver-form/driver-form.component";
+import { CustomerFormComponent } from "./../customer-form/customer-form.component";
+import { VehicleStockFormComponent } from "./../vehiclestock-form/vehiclestock-form.component";
+import { NavFormComponent } from "./../nav-form/nav-form.component";
+import { ActivitiesComponent } from "./../activities/activities.component";
+import { NavigationBarComponent } from "./../navigation-bar/navigation-bar.component";
+import { DashboardComponent } from "./../dashboard/dashboard.component";
+import { ProfileComponent } from "../profile/profile.component";
+import { GeneralService } from "../../shared/services/GeneralService";
+import { UserDairyModule } from "../calendar/user-dairy.module";
 
+import { NgbTooltipModule } from "@ng-bootstrap/ng-bootstrap";
 @NgModule({
   imports: [
     TableListRoutingModule,
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    UserDairyModule
+    UserDairyModule,
+    NgbTooltipModule
   ],
   declarations: [
     DashboardComponent,
@@ -51,8 +53,8 @@ import { UserDairyModule } from '../calendar/user-dairy.module';
     NavFormComponent,
     NavigationBarComponent
   ],
-  exports: [ TableListComponent ],
-  entryComponents: [ TabComponent ],
-  providers: [ GeneralService ]
+  exports: [TableListComponent],
+  entryComponents: [TabComponent],
+  providers: [GeneralService]
 })
 export class TableListModule {}
