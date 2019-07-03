f4673f59b4012a7bfc770582db65559fb95aa30b
Mukeshkumar <mukesh@infohawk.in>
2019-07-03 13:28:11 +0530

    Serial number and fontsize changes

--- src/app/shared/services/AuthService.ts
+++ src/app/shared/services/AuthService.ts
@@ -1,13 +1,13 @@
 // src/app/auth/auth.service.ts
 
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable()
 export class AuthService {
   // ...
   public isAuthenticated(): Boolean {
-    const token = localStorage.getItem('access_token');
-    console.log('Login Token: ', token);
+    const token = localStorage.getItem("access_token");
+    console.log("Login Token: ", token);
     // Check whether the token is expired and return
     // true or false
     return token ? true : false;
